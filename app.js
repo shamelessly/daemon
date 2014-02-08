90622ee6f6eb215dfb25fec56415ebb2462c48b2
Jean-Baptiste PIN <jean-baptiste.pin@keatonrow.com>
2014-02-08 18:26:40 -0500

    added sw montreal + template tweets

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var twit = require('twit');
 var monk = require('monk');
 var Model = require('./model');
 var accounts = require('./account');
+var template = require('./tweets_template');
+var _ = require('lodash');
 
 var tweets = [];
+var users = [];
 
 function sendTweet(text, idstr){
   app.twclient.post('statuses/update', {status : text, in_reply_to_status_id: idstr}, function(err){
@@ -16,9 +19,11 @@ function sendTweet(text, idstr){
 var app = {
   model : new Model(),
 
-  init: function(){
+  init: function(cb){
     if(!app.model.ready){
-      return setTimeout(app.init, 100);
+      return setTimeout(function(){
+        app.init(cb);
+      }, 100);
     }
     app.twclient = new twit({
       consumer_key: 't4jGhlS0GGUfXsHENh1rDg',
@@ -26,24 +31,35 @@ var app = {
       access_token: '2333915352-RFAMKRURCYm2HjY1FOjv2BB6p5qeoRC4xb0yZ6D',
       access_token_secret: 'T1SukUJx0YyHeMXg6LFirEL0jklI0CYCPx20KECO1qsyJ'
     });
-    setTimeout(app.start, 1);
+    setTimeout(cb, 1);
   },
+
+  getList : function(){
+    console.log('get list...');
+    app.twclient.get('friends/ids', function(err, result){
+      app.twclient.get('users/lookup', {user_id : result.ids.join(',')}, function(err, result){
+        result.forEach(function(item){
+          console.log('"@' + item.screen_name + '",');
+        });
+      });
+    });
+  },
+
   start: function(){
     console.log('Start listening...');
-
 
     var stream = app.twclient.stream('statuses/filter', { track: accounts.join(','), language: 'en, fr, us' });
 
     stream.on('tweet', function (tweet) {
       var index = tweets.push(tweet);
-      console.log(index-1, tweet.user.screen_name, tweet.text);
+      console.log(index-1, 'followers:', tweet.user.followers_count, 'friends',tweet.user.friends_count, tweet.user.screen_name, tweet.text);
     });
 
     process.stdin.resume();
     process.stdin.setEncoding('utf8');
      
     process.stdin.on('data', function (chunk) {
-      var index = tweets.length;
+      var index = tweets.length-1;
       if(chunk && chunk !== null && chunk !== ""){
         index = parseInt(chunk, 10);
       }
@@ -54,9 +70,16 @@ var app = {
       if(!tweet){
         return;
       }
-
       app.model.saveTweet(tweets[index], function(err, result){
         var brand = [];
+        tweet.entities.hashtags.forEach(function(h){
+          if(h.text.toLowerCase() === 'swmontreal' && users.indexOf(tweet.user.screen_name) === -1) {
+            users.push(tweet.user.screen_name);
+            return setTimeout(function(){
+              sendTweet("Hey, @"+tweet.user.screen_name+", so good to hear what you had to say about #SWMontreal. Check out what we did with it!" , tweet.id_str);
+            }, 6000);
+          }
+        });
         tweet.entities.user_mentions.forEach(function(m){
           accounts.forEach(function(a){
             if(a.substring(1).toLowerCase() == m.screen_name.toLowerCase()){
@@ -65,13 +88,12 @@ var app = {
           });
         });
         if(brand.length){
-          brand.forEach(function(b){
-            console.log(require('util').inspect(b, true, 10, true));
-            var text = "Hey @" + tweet.user.screen_name + " we just add your feedback about " + b + " on our website. Have a look ! http://shamelessly.co";
-            setTimeout(function(){
-              sendTweet(text, tweet.id_str);
-            }, 6000);
-          });
+          var text = _(template).shuffle().at(0).value().toString();
+          text = text.replace('[customer]', tweet.user.screen_name);
+          text = text.replace('[brand]', brand[0]);
+          setTimeout(function(){
+            sendTweet(text, tweet.id_str);
+          }, 6000);
         }
       });
     });
@@ -79,4 +101,5 @@ var app = {
   stop: function(){}
 };
 
-app.init();
+app.init(app.start);
+// app.init(app.getList);
---
+++ tweets_template.js
@@ -0,0 +1,10 @@
+var templates = [
+  "Hey @[customer] we just added your feedback about @[brand] on our website. Check it out! http://shamelessly.co",
+  "Hey @[customer], awesome to hear what you have to say about @[brand]. We posted it here! Check it out http://shamelessly.co",
+  "Hey @[customer], gotta love your feedback about @[brand]. We shared it here. See why. http://shamelessly.co",
+  "@[brand] looks like [customer] has something to say about you here. http://shamelessly.co",
+  "Hey, @[customer], so good to hear what you had to say about @[brand]. Check out what we did with it! http://shamelessly.co",
+  "So... hummm... @[brand] what do you have to say about that now? http://shamelessly.co"
+];
+
+module.exports = templates;
