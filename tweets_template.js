c16c8fdc48bf5d0b69164da58d4a7e88893fd1cc
Jean-Baptiste PIN <jean-baptiste.pin@keatonrow.com>
2014-02-09 10:34:23 -0500

    fix type

--- tweets_template.js
+++ tweets_template.js
@@ -1,8 +1,8 @@
 var templates = [
   "Hey @[customer] we just added your feedback on our website. Check it out! http://shamelessly.co",
   "Hey @[customer], awesome to hear what you have to say on brands. We posted it here! Check it out http://shamelessly.co",
-  "Hey @[customer], gotta love your feedback on brand. We shared it here. See why. http://shamelessly.co",
-  "[customer] has something to say about its relationship with brand. http://shamelessly.co",
+  "Hey @[customer], gotta love your feedback on the brand. We shared it here. See why. http://shamelessly.co",
+  "[customer] has something to say about its relationship with the brand. http://shamelessly.co",
   "Hey, @[customer], so good to hear what you had to say about [brand]. Check out what we did with it! http://shamelessly.co",
   "So... hummm... [brand] what do you have to say about that now? http://shamelessly.co"
 ];
@@ -12,8 +12,8 @@ var swmontreal = [
   "So you are finally here @[customer]. Please take time to check out our project. http://shamelessly.co",
   "@[customer] good to hear from you. You gotta love our concept. http://shamelessly.co",
   "Hey @[customer], have you seen http://shamelessly.co. It's a #SWMontreal project. Feel free to visit us.",
-  "We will love to see you @[customer] and having your feedback, come to visit us. http://shamelessly.co",
-  "Have you never had a bad customer service experience @[customer]? We will love to hear from you. http://shamelessly.co"
+  "We would love to see you @[customer] and having your feedback, come visit us. http://shamelessly.co",
+  "Have you never had a bad customer service experience @[customer]? We would love to hear from you. http://shamelessly.co"
 ];
 
 module.exports = {
